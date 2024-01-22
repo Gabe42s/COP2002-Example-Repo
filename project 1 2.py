e8725de8b03e8ad3a4f4b2a96c731312fb4d0048
Gabe42s <157416485+Gabe42s@users.noreply.github.com>
2024-01-22 15:25:29 -0500

    Update project 1 2.py

--- project 1 2.py
+++ project 1 2.py
@@ -1,7 +1,6 @@
-Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> # Creating a project for GitHub
->>> #By Gabe Fortus
->>> 
->>> print("COP2002 Fall 2030")
-COP2002 Fall 2030
+# Creating a project for GitHub 
+#By Gabe Fortus 
+
+print("COP2002 Fall 2030")
+
+print("I'm understanding how to use GitHub to keep track of changes!")
